5a583940ad4f9e60f9336760e00efa60e1e167d4
jiacs <jiacsen@qq.com>
2025-06-23 06:03:35 +0800

    task2-subtask2-v2
    中间忘记提交了
    1.实现了get_item_info方法，并且注意到，python中的值和对象是完全一样的，对象即是值，值亦是对象。
    2.完善任务2.1子任务，将魔术方法做完特殊类型处理，使separate_by_type方法更完善。
    3.添加is_magic函数，用于判断是否是魔术方法。

--- class_lib.py
+++ class_lib.py
@@ -7,25 +7,13 @@ Created on 2025/6/23 3:12
 import importlib
 import inspect
 
+def is_magic(name):
+    if len(name) < 4:
+        return False
+    if name[:2] != '__' or name[-2:] != '__':
+        return False
+    return True
 
-# TODO:第二个学习任务
-# 现在你已经掌握了基础，让我们增加功能。下一个任务：
-# 为LibraryExplorer类添加两个新方法：
-# 1.
-# separate_by_type() - 返回一个字典，将模块中的项目按类型分类：
-#
-# 键：'functions'（函数）、'classes'（类）、'variables'（变量/常量）
-# 值：对应类型的项目名称列表
-#
-# 2.
-# get_item_info(item_name) - 接收一个项目名称，返回该项目的详细信息（类型、值等）
-#
-# 学习提示：
-#
-# 使用callable()检查是否为可调用对象（函数/方法）
-# 使用inspect.isclass()检查是否为类（需要import inspect）
-# 使用getattr()获取对象的属性值
-# 考虑异常处理：如果项目名称不存在怎么办？
 class LibraryExplorer:
     def __init__(self, module_name):
         self.module = importlib.import_module(module_name)
@@ -37,19 +25,41 @@ class LibraryExplorer:
         return len(dir(self.module))
 
     def separate_by_type(self):
-        types_dict = {'functions': [], 'classes': [], 'variables': []}
+        types_dict = {
+            'functions': [],
+            'classes': [],
+            'special': [],
+            'variables': []
+        }
         for item_name in self.list_all_items():
-            if inspect.isclass(getattr(self.module, item_name)):  # 首先判断是否为类
+            obj = getattr(self.module, item_name)
+            if inspect.isclass(obj):  # 首先判断是否为类
                 types_dict['classes'].append(item_name)
-            elif callable(getattr(self.module, item_name)):  # 根据是否为可调用对象判断是否为函数，类也是可调用对象，但已经在上一步判断过了
+            elif callable(obj):  # 根据是否为可调用对象判断是否为函数，类也是可调用对象，但已经在上一步判断过了
                 types_dict['functions'].append(item_name)
+            elif is_magic(item_name):
+                types_dict['special'].append(item_name)
             else:
                 types_dict['variables'].append(item_name)
         return types_dict
+
+    def get_item_info(self, item_name):
+        if not item_name in self.list_all_items():
+            print('Item not found.')
+            return None
+        else:
+            obj = getattr(self.module, item_name)
+            info_dict = {
+                'name': item_name,
+                'type': type(obj).__name__,
+                'value': obj,
+                'doc': obj.__doc__
+            }
+            return info_dict
 
 
 if __name__ == '__main__':
     pak_name = 'math'
     Explorer = LibraryExplorer(pak_name)
-    print(Explorer.list_all_items())
-    print(Explorer.count_items())
+    print(Explorer.separate_by_type())
+    print(Explorer.get_item_info('pi'))
